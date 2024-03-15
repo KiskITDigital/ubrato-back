410ed9bfaad2f7b6c9edefb96bdca1c6e83e0e8a
g0dm0d <ivan.berlin06@gmail.com>
2024-03-15 20:36:34 +0300

    upd migration

--- app/repositories/migration/1710349476_create_tender_table.down.sql
+++ app/repositories/migration/1710349476_create_tender_table.down.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS tender;
 DROP FUNCTION IF EXISTS update_tender_document_tsv;
-DROP TRIGGER IF EXISTS tsvector_update_tender;
+DROP TRIGGER IF EXISTS tsvector_update_tender ON tender;
