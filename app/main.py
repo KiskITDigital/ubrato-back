879ab811e1437295135eb403ebde002fd77f57f6
g0dm0d <ivan.berlin06@gmail.com>
2024-03-21 19:41:07 +0300

    cors update

--- app/main.py
+++ app/main.py
@@ -21,12 +21,18 @@ app = FastAPI(
     ],
 )
 
+origins = [
+    "http://apiubrato.godmod.dev",
+    "https://apiubrato.godmod.dev",
+    "http://localhost",
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
