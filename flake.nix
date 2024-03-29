26956e7af7876b45378e05d7ac5da1dccc301d42
g0dm0d <ivan.berlin06@gmail.com>
2024-03-29 17:52:30 +0300

    upd ci

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [ python311 virtualenv poetry go-migrate ruff ] ++
-            (with pkgs.python311Packages; [ pip psycopg2 python-lsp-server pyls-isort pyls-flake8 ]);
+            (with pkgs.python311Packages; [ pip psycopg2 python-lsp-server pyls-isort pyls-flake8 pylsp-mypy mypy ]);
         };
       });
     };
