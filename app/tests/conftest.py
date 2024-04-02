7ff770d229c14976faac3165a52a2960b85b0623
g0dm0d <ivan.berlin06@gmail.com>
2024-04-02 21:01:12 +0300

    test

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -18,21 +18,6 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
 
-@pytest.fixture(scope="session")
-def docker_compose_file(pytestconfig):
-    return os.path.join(str(pytestconfig.rootdir), "docker-compose.yml")
-
-
-@pytest.fixture(scope="session")
-def docker_compose_command() -> str:
-    return "docker-compose"
-
-
-@pytest.fixture(scope="session")
-def docker_cleanup() -> str:
-    return "down"
-
-
 def is_responsive(docker_ip, port):
     try:
         psycopg2.connect(
