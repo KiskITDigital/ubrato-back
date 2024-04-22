81a800003de58cbedf401b427e66e9b22f7e05af
g0dm0d <ivan.berlin06@gmail.com>
2024-04-22 17:42:50 +0300

    code refactoring

--- app/tools/egrul.py
+++ app/tools/egrul.py
@@ -8,28 +8,30 @@ EGRUL_URL = "https://egrul.nalog.ru/"
 
 def get_org_by_query(query: str) -> List[models.EgrulCompany]:
     form_data = {
-        'vyp3CaptchaToken': '',
-        'page': '',
-        'query': query,
-        'region': '',
-        'PreventChromeAutocomplete': ''
+        "vyp3CaptchaToken": "",
+        "page": "",
+        "query": query,
+        "region": "",
+        "PreventChromeAutocomplete": "",
     }
 
     response = requests.post(EGRUL_URL, data=form_data)
 
-    response = requests.get(EGRUL_URL+"search-result/" + response.json()['t'])
+    response = requests.get(
+        EGRUL_URL + "search-result/" + response.json()["t"]
+    )
 
     companies: List[models.EgrulCompany] = []
 
-    for company_data in response.json()['rows']:
+    for company_data in response.json()["rows"]:
         company = models.EgrulCompany(
-            name=company_data['c'],
-            director=company_data['g'],
-            inn=company_data['i'],
-            kpp=company_data['p'],
-            ogrn=company_data['o'],
-            registration_date=company_data['r'],
-            region=company_data['rn']
+            name=company_data["c"],
+            director=company_data["g"],
+            inn=company_data["i"],
+            kpp=company_data["p"],
+            ogrn=company_data["o"],
+            registration_date=company_data["r"],
+            region=company_data["rn"],
         )
 
         companies.append(company)
